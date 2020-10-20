27a83d5974de5d4827d9453510fb85dede279c06
ArtDark <artdark@icloud.com>
2020-10-20 08:50:52 +0300

    rewrite

--- cmd/stats/main.go
+++ cmd/stats/main.go
@@ -3,55 +3,72 @@ package main
 import (
 	"fmt"
 	"github.com/ArtDark/bgo_goroutines/pkg/stats"
+	"log"
+	"os"
+	"runtime/trace"
+	"sync"
 	"time"
 )
 
 func main() {
-	////runtime.GOMAXPROCS(4)
-	//
-	//f, err := os.Create("trace.out")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer func() {
-	//	if err := f.Close(); err != nil {
-	//		log.Print(err)
-	//	}
-	//}()
-	//err = trace.Start(f)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer trace.Stop()
+	//runtime.GOMAXPROCS(4)
+
+	f, err := os.Create("trace.out")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Print(err)
+		}
+	}()
+	err = trace.Start(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer trace.Stop()
 
 	const users = 10_000_000
 	const transactionsPerUser = 100
 	const transactionAmount = 1_00
 	transactions := make([]int64, users*transactionsPerUser)
-
 	for index := range transactions {
 		// для простоты храним только суммы
 		// и считаем, что каждая транзакция = 1 руб.
 		transactions[index] = transactionAmount
 	}
 
-	total := int64(0)
+	start := time.Date(2015, 4, 1, 0, 0, 0, 0, time.Local)
+	finish := time.Date(2016, 4, 1, 0, 0, 0, 0, time.Local)
 
-	start := time.Date(2016, 1, 2, 0, 0, 0, 0, time.Local)
-	finish := time.Date(2020, 4, 1, 0, 0, 0, 0, time.Local)
-
-	months := make([]int64, 0)
-
+	months := make([]string, 0)
 	next := start
 	for next.Before(finish) {
-		months = append(months, next.Unix())
+		months = append(months, fmt.Sprintf("%d-%d", next.Year(), next.Month()))
 		next = next.AddDate(0, 1, 0)
 	}
-	months = append(months, finish.Unix())
-	fmt.Println(len(months))
 
-	stats.SumConcurrently(transactions, len(months))
+	monthsMap := make(map[string][]int64)
+	partsCount := len(months)
+	partSize := len(transactions) / partsCount
 
-	time.Sleep(time.Minute)
-	fmt.Println("Total:", total)
+	for i := 0; i < len(months); i++ {
+		monthsMap[months[i]] = transactions[i*partSize : (i+1)*partSize]
+	}
+
+	total := int64(0)
+
+	wg := sync.WaitGroup{}
+
+	for _, i := range monthsMap {
+		wg.Add(1)
+		go func() {
+			total += stats.Sum(i)
+			wg.Done()
+		}()
+
+	}
+
+	fmt.Println("Sum", total)
+
 }
